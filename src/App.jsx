cab99cb160b0318ac64ad1d242803dc856b2df76
ashif shaik <110083426+ash212-hub@users.noreply.github.com>
2024-11-06 11:43:02 +0530

    Update App.jsx_2

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
       
     </Canvas>
     <div>
-      <button className={` ${play? `bg-blue-400 `:`bg-red-500`} text-white ml-4 rounded-xl p-2 relative top-[89vh] mb-4`} onClick={playhandle}>
+      <button className={` ${play? `bg-blue-400 `:`bg-red-500`} text-white ml-4 rounded-xl p-2 absolute top-[89vh] mb-4`} onClick={playhandle}>
         {console.log(play)}
         {play ?" play ":" stop"}
       </button>
